cb18c35325e3dc8d01085235963760c53047c4ca
elokaagu <eloka.agu@icloud.com>
2021-07-06 22:33:30 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Eloka Agu on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
